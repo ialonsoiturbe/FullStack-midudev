4762cafd08a71e72c50a8a47e2fc749409df694d
Inés Alonso Iturbe <109752601+ialonsoiturbe@users.noreply.github.com>
2026-01-20 20:29:46 +0100

    script separado

---
+++ ejercicios_js/script.js
@@ -0,0 +1,6 @@
+
+const boton = document.querySelector('#boton-importante')
+
+boton.addEventListener('click', function responseClick() {
+    alert('Gracias por tu interés. Pronto nos pondremos en contacto contigo')
+})
