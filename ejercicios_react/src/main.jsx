bd6051d4f167c4102c468e8833418c3482dd9dc4
ialonsoiturbe <ines@alonsoiturbe.net>
2026-09-09 19:49:12 +0200

    main jsx

--- ejercicios_react/src/main.jsx
+++ ejercicios_react/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Renderiza la aplicación en el div id = root
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
