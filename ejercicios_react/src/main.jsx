57a1e07b0bed9c5203a544249789f446c1a35696
ialonsoiturbe <ines@alonsoiturbe.net>
2026-09-09 19:50:36 +0200

    main jsx

--- ejercicios_react/src/main.jsx
+++ ejercicios_react/src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 //Renderiza la aplicación en el div id = root
 ReactDOM.createRoot(document.getElementById('root')).render(
+    // StrictMode es un componente que nos ayuda a detectar problemas en nuestra aplicación
+    //Sólo funciona en desarrollo, no en producción
+    //Renderiza los componentes dos veces para detectar efectos secundarios
   <React.StrictMode>
     <App />
   </React.StrictMode>,
