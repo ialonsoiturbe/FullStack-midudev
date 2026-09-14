8fa118177ecd10dd5e3a299f92b6feda13f2d2a6
ialonsoiturbe <ines@alonsoiturbe.net>
2026-09-14 20:57:02 +0200

    header

--- ejercicios_react/src/components/Header.jsx
+++ ejercicios_react/src/components/Header.jsx
@@ -1,17 +1,15 @@
 function Header() {
     return (
-        <header>
-            <nav>
-                <div className="logo">
-                    <h1>DevJobs</h1>
-                </div>
-
-                <ul>
-                    <li><a href="#jobs">Trabajos</a></li>
-                    <li><a href="#about">Acerca</a></li>
-                    <li><a href="#contact">Contacto</a></li>
-                </ul>
-            </nav>
+        <header className="header">
+            
+            <div  className="container">
+               <h1>DevJobs</h1>
+                <nav>
+                    <a href="#jobs">Trabajos</a>
+                    <a href="#about">Acerca</a>
+                    <a href="#contact">Contacto</a>
+                </nav>
+            </div>
         </header>
     )
 }
