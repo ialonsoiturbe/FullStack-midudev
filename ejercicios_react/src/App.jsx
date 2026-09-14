4a38918c9f051a7be4b70aeaffd3e2b4063035a0
ialonsoiturbe <ines@alonsoiturbe.net>
2026-09-14 20:04:41 +0200

    appsjx

--- ejercicios_react/src/App.jsx
+++ ejercicios_react/src/App.jsx
@@ -1,22 +1,19 @@
 import { UseState } from "react";
 import './App.css'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 //Definimos el componente principal de la app
 function App() {
   return (
-    <div>
-      {/*-- Aquí iría todo tu HTML de DevJobs */}
-      <header>
-        <nav>...</nav>
-      </header>
-
+    <div className="app">
+      <Header />
       <main>
-        <section>...</section>
+        <h2>Bienvenido a DevJobs</h2>
+        <p>Encuentra las mejores oportunidades de trabajo en desarrollo web y tecnología.</p>
       </main>
-
-      <footer>
-        <p>...</p>
-      </footer>
+      <Footer />
     </div>
   )
 }
