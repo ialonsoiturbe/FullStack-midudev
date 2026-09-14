14615c6046eb9291844c2208762a48e8efc9ddc3
ialonsoiturbe <ines@alonsoiturbe.net>
2026-09-14 20:03:11 +0200

    footer

---
+++ ejercicios_react/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>&copy; 2024 DevJobs. Todos los derechos reservados.</p>
+
+                <div className="social-links">
+                    <a href="#twitter">Twitter</a>
+                    <a href="#linkedin">LinkedIn</a>
+                    <a href="#github">GitHub</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+//exportamos el componente para poder usarlo en otros archivos como App.jsx
+export default Footer
